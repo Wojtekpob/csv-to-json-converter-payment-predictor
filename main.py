3fe0b7d7ab1591e0f8d453f1a32a652a0c3963c1
wojtek <wojtekpobocha@gmail.com>
2023-01-21 21:46:08 +0100

    removed prints and closed convertion in function

--- main.py
+++ main.py
@@ -13,20 +13,19 @@ def write_to_json(file_handle, invoice_list):
 
 def read_from_csv(file_handle):
     reader = csv.DictReader(file_handle,delimiter=';')
-    print(reader.fieldnames)
     records_list=[]
     for row in reader:
-        print(row)
         records_list.append(row)
 
     return records_list
 
 
-with open('test_payment_predictor.csv') as handle:
-    invoice_list=read_from_csv(handle)
-    handle.close()
+def convert_from_csv_to_json(csv_file__to_convert_from,json_file_to_convert_to):
+    with open(csv_file__to_convert_from) as handle:
+        invoice_list=read_from_csv(handle)
+        handle.close()
+    with open(json_file_to_convert_to,'w') as json_handle:
+        write_to_json(json_handle,invoice_list)
+        json_handle.close()
 
-
-with open('converted_csv_file.json','w') as json_handle:
-    write_to_json(json_handle,invoice_list)
-    json_handle.close()
+convert_from_csv_to_json('test_payment_predictor.csv','converted_csv_file.json')
